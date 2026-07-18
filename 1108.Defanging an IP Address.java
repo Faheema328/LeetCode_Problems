a4a6d5cf5349c3af4925dc58fb3a0ef81d2a0f34
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 10:28:08 +0530

    Update 1108.Defanging an IP Address.java

--- 1108.Defanging an IP Address.java
+++ 1108.Defanging an IP Address.java
@@ -1,5 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replace(".", "[.]");
+        return address.replace(".", "[.]");//stringName.replace("old value","new value")
     }
 }
+//Defanging-->Converting an IP address into safe format by replacing "." with "[.]"
