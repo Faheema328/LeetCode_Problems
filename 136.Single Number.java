c6d2d8784031422f8bb2bcff910776cf4e1b8e68
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 13:39:24 +0530

    Update 136.Single Number.java

--- 136.Single Number.java
+++ 136.Single Number.java
@@ -1,9 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result=0;
-        for(int num : nums){
-            result=result^num;
+        for(int i=0;i<nums.length;i++){
+            result=result^nums[i];
         }
-        return result;
+        return result;//All duplicate elements become 0,leaving only the single element
     }
 }
+/* Practical Approach:
+    Every element appears twice except one element
+    XOR of two same numbers is 0
+    XOR of 0 with any number gives that number
+    XOR of two different numbers gives a new number based on their binary bits
+    XOR all the elements to get the single element */
