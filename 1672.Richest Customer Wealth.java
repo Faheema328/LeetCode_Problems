4f92cf0f3a567af936fc538bc65527a39abbe539
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 11:22:30 +0530

    Update 1672.Richest Customer Wealth.java

--- 1672.Richest Customer Wealth.java
+++ 1672.Richest Customer Wealth.java
@@ -1,10 +1,11 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
         int maxWealth=0;
-        for(int i=0;i<accounts.length;i++){
+        for(int i=0;i<accounts.length;i++){//Visit each customer(Each row represents one customer)
             int totalWealth=0;
-            for(int j=0;j<accounts[i].length;j++){
-               totalWealth=totalWealth+accounts[i][j];
+            //Visit each bank account(Each column represents one bank)
+            for(int j=0;j<accounts[i].length;j++){//accounts[i].length-->No. of banks ith customer has
+            totalWealth=totalWealth+accounts[i][j];//accounts[i][j]-->ith customers' money in jth bank
             }
             if(totalWealth>maxWealth){
                 maxWealth=totalWealth;
@@ -13,3 +14,8 @@ class Solution {
         return maxWealth;
     }
 }
+
+/* Practical Approach:
+    Calculate the total wealth of each customer by adding all their bank balances
+    Compare each customer's wealth with the maximum wealth found so far
+    Return the maximum wealth */
