5fc1c79ff4af77d2c9ec2bd1d0feacd977bdc9b7
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 13:11:56 +0530

    Update 35.Search Insert Position.java

--- 35.Search Insert Position.java
+++ 35.Search Insert Position.java
@@ -4,10 +4,15 @@ class Solution {
             if(nums[i]==target){
                 return i;
             }
-            if(nums[i]>target){
-                return i;
+            if(nums[i]>target){//Target should be inserted at this index
+                return i
             }
         }
-        return nums.length;
+        return nums.length;//Target is greater than all elements-->so insert at the end
     }
 }
+/*Practical Approach:
+   The question asks for the index of the target in the sorted array
+   If the target is found, return its index
+   If the current element becomes greater than the target, return its index because the target should be inserted before it
+   If no such element is found, return nums.length because the target should be inserted at the end */
