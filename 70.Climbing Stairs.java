74184b7dad1c9f52fa1e8deeda69bb5a9d5da2b3
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 12:58:28 +0530

    Improve climbing stairs solution and documentation
    Refactor climbing logic and add comments for clarity.

--- 70.Climbing Stairs.java
+++ 70.Climbing Stairs.java
@@ -8,11 +8,18 @@ class Solution {
        }
        int first=2;
        int second=3;
-       for(int i=4;i<=n;i++){
+
+       for(int i=4;i<=n;i++){//Calculate the number of ways from stair 4 to n
+        //Current ways = previous two ways
         int current=first+second;
-        first=second;
+        first=second;//Update 1st and 2nd
         second=current;
        }
-       return second;
+       return second;//second holds the final answer after the loop ends(bcoz current goes out of scope after loop ends)
     }
 }
+/* Practical Approach:
+    The question asks for the total number of distinct ways to reach the nth stair
+    You can climb either 1 stair or 2 stairs at a time
+    The number of ways to reach the current stair is the sum of the previous two stairs
+    Keep updating the last two answers until you reach the nth stair */
