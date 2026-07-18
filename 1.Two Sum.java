1c7dc65a243a2ef156f80dd333cb147514f9f70f
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 10:58:15 +0530

    Update 1.Two Sum.java

--- 1.Two Sum.java
+++ 1.Two Sum.java
@@ -4,9 +4,15 @@ class Solution {
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
                     return new int[]{i,j};
+                    /* Java doesn't accept returning array literals
+                    So,we must create a new array */
                 }
             }
         }
-        return new int[]{};
+        return new int[]{};//When no pair is found
     }
 }
+
+/* j=i+1 bcoz:
+>Same element shoud not be used twice
+>Avoids checking same pair twice */
