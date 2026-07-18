3611cba22fdf6e77c29116800e1c26d7832e1d49
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 10:24:36 +0530

    Update 9.Palindrome Number.java

--- 9.Palindrome Number.java
+++ 9.Palindrome Number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
+        if(x<0){//Negative numbers are not palindromes(-121 != 121-)
+            return false;//121- is an invalid integer representation
         }
         int original=x;
         int rev=0;
