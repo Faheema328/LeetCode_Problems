ed4fa28ac113e2531ccb8fadac6186865e5a6ed0
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-18 11:45:43 +0530

    Update 1480.Running Sum of 1d Array.java

--- 1480.Running Sum of 1d Array.java
+++ 1480.Running Sum of 1d Array.java
@@ -1,10 +1,16 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int reqArr[]=new int[nums.length];
-        reqArr[0]=nums[0];
+        nums[0]=reqArr[0];
         for(int i=1;i<reqArr.length;i++){
-            reqArr[i]=reqArr[i-1]+nums[i];
+            reqArr[i]=reqArr[i-1]+nums[i];//Current element(reqArr)=Previous running sum + current element(nums)
         }
         return reqArr;
     }
 }
+
+/* Practical Approach:
+        Create a new array of the same size as the input array.
+        The first element remains the same because there are no previous elements
+        For every other element,add the previous running sum(of the reqArr) with the current element(of nums)
+        Return the running sum array */
