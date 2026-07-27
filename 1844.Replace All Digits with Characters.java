aef13ca3ab508ec41d47d90c60f4f114add86e31
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-27 13:02:38 +0530

    Update 1844.Replace All Digits with Characters.java

--- 1844.Replace All Digits with Characters.java
+++ 1844.Replace All Digits with Characters.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public String replaceDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                sb.append(s.charAt(i));
+            } else {
+                char shifted = (char) (s.charAt(i - 1) + (s.charAt(i) - '0'));
+                sb.append(shifted);
+            }
+        }
+        return sb.append('\0').deleteCharAt(sb.length()-1).toString();
+    }
+}
