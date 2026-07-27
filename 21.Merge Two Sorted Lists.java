d137bafaad76a33daca933c7e3dce5a7b852cfa5
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-27 11:30:47 +0530

    Create 21.Merge Two Sorted Lists.java

---
+++ 21.Merge Two Sorted Lists.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = (list1 != null) ? list1 : list2;
+        return dummy.next;
+    }
+}{
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (!set.add(n)) return true;
+        }
+        return false;
+    }
+}
