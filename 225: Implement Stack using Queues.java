8bbfce33497c4a90d3d2ede5c12b0298d5d10fe4
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-07-27 11:22:26 +0530

    Update 225: Implement Stack using Queues.java

--- 225: Implement Stack using Queues.java
+++ 225: Implement Stack using Queues.java
@@ -1,13 +1,27 @@
-class MyStack:
-    def __init__(self):
-        self.q = []
-    def push(self, x):
-        self.q.append(x)
-        for _ in range(len(self.q)-1):
-            self.q.append(self.q.pop(0))
-    def pop(self):
-        return self.q.pop(0)
-    def top(self):
-        return self.q[0]
-    def empty(self):
-        return len(self.q) == 0
+class MyStack {
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            q.add(q.poll());
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
