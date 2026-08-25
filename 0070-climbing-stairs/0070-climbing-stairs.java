e9670e1ba66adf200e2c7f826381d037edc132f8
Shaik Faheemunnisa <faheemashaik328@gmail.com>
2026-08-25 09:42:39 +0530

    Time: 0 ms (100%), Space: 42 MB (73.21%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int climbStairs(int n) {
+       if(n==1){
+        return 1;
+       }
+       if(n==2 || n==3){
+        return n;
+       }
+       int first=2;
+       int second=3;
+
+       for(int i=4;i<=n;i++){//Calculate the number of ways from stair 4 to n
+        //Current ways = previous two ways
+        int current=first+second;
+        first=second;//Update 1st and 2nd
+        second=current;
+       }
+       return second;//second holds the final answer after the loop ends(currents goes out of scope after loop ends)
+    }
+}
+/* Practical Approach:
+    The question asks for the total number of distinct ways to reach the nth stair
+    You can climb either 1 stair or 2 stairs at a time
+    The number of ways to reach the current stair is the sum of the previous two stairs
+    Keep updating the last two answers until you reach the nth stair */
